5d319db7b9428e4243a253430fb5dfdd843bca0a
tanbui1 <99457061+tanbui1@users.noreply.github.com>
2026-06-05 01:35:47 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => ({
   base: '/chapter3-demo/',
-})
+  plugins: [react()],
+}));
